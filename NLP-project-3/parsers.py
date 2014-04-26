399dd5f7cceaea24ffd27b6408623a8f286967ff
Dominick Twitty <dktwitty@gmail.com>
2014-04-26 13:02:28 -0400

    whatever

--- NLP-project-3/parsers.py
+++ NLP-project-3/parsers.py
@@ -1,3 +1,5 @@
+import re
+from collections import defaultdict
 class ReviewSentence():
 	def __init__(self, line):
 		self.sentiment = line[:3]
@@ -41,5 +43,49 @@ def parse_review_file(filename = "training_data.txt"):
 	return reviews
 
 
-reviews = parse_review_file()
-r = reviews[0]
+# reviews = parse_review_file()
+# r = reviews[0]
+
+def get_pos_neg_words():
+	pos = set()
+	neg = set()
+	with open('opinion-lexicon-English/positive-words.txt', 'r') as f:
+		for line in f:
+			if line[0] != ';':
+				pos.add(line.strip())
+	with open('opinion-lexicon-English/negative-words.txt', 'r') as f:
+		for line in f:
+			if line[0] != ';':
+				neg.add(line.strip())
+	return pos, neg
+
+clue_re = re.compile(r"type=(.*) len=1 word1=(.*) pos1=(.*) stemmed1=(.*) priorpolarity=(.*)")
+class ClueDict:
+	def __init__(filename):
+		self.clues = defaultdict(lambda : [])
+		with open(filename, 'r') as f:
+			for line in f:
+				s = line.strip()
+				clue = Clue(s)
+				self.clues[clue.word].append(clue)
+	def __contains__(self, word):
+		return word in self.clues
+	def __getitem__(self, word):
+		return self.clues[word]
+
+
+class Clue:
+	def __init__(line):
+		match = clue_re.match(line)
+		groups = match.groups()
+		self.strong = (groups[0] == 'strongsubj')
+		self.word = groups[1]
+		self.pos = groups[2]
+		self.stemmed = (groups[3] == 'y')
+		self.polarity = groups[4][:3]
+	def __str__(self):
+		return "word: %s, polarity: %s" % (self.word, self.polarity)
+	def __repr__(self):
+		return str(self)
+
+
--- NLP-project-3/review_classifier.py
+++ NLP-project-3/review_classifier.py
@@ -3,7 +3,7 @@ from nltk.model.ngram import NgramModel
 from nltk import wordpunct_tokenize
 from nltk.stem.lancaster import LancasterStemmer
 import nltk
-from parsers import parse_review_file
+from parsers import *
 from operator import itemgetter
 from collections import defaultdict
 from math import sqrt
@@ -30,51 +30,23 @@ def preprocess(sentence):
     converts a sentence into a list of lowercse, lemmatized words
     """
     st = LancasterStemmer()
-    ret = [lemmatize(w.lower()) for w in parse_words_only(sentence)]
+    ret = [w.lower() for w in parse_words_only(sentence)]
     return ret
 
 training_reviews = parse_review_file("training_data.txt")
 
-sentences = {'pos' : [], 'neu' : [], 'neg' : []}
-
-for review in training_reviews:
-    for sentence in review:
-        sent = sentence.sentiment
-        text = preprocess(sentence.text)
-        sentences[sent].append(text)
-
-word_scores = defaultdict(lambda : 0)
-for sent in ['pos', 'neu', 'neg']:
-    for s in sentences[sent]:
-        text = preprocess(sentence.text)
-        for word in text:
-            if sent == 'pos':
-                add = 1
-            elif sent == 'neu':
-                add = 0
-            else:
-                add = -1
-            word_scores[word] += add
-
-def mean(l):
-    s = 0.0
-    n = 0
-    for i in l:
-        s += i
-        n += 1
-    return s / n
-
-
-mean_word_score = mean(word_scores.values())
-std_dev = sqrt(mean(i ** 2 for i in word_scores.values()) - mean_word_score ** 2)
+clue_dict = ClueDict("subjectivity_clues/clues.tff")
 
 def classify(sentence):
+    score = 0
+    for word in sentence:
+        if word in clue_dict:
+            clues = clue_dict[word]
+            clue = clues[0]
     
-
-
 def label_review(review):
     labelled_sequence = []
-    for senetence in review:
+    for sentence in review:
         symb = classify(preprocess(sentence.text))
         sentiment = sentence.sentiment
         labelled_sequence.append((sentiment, symb))
@@ -91,6 +63,7 @@ def tag(review):
     symbs = map(classify, sentences)
     path = hmm.best_path(symbs)
     print [s.sentiment for s in review]
+    print map(classify, sentences)
     print path
 
 validation_reviews = parse_review_file("validation_data.txt")
